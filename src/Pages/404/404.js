bfc4db2542b2ff1f9936afc5da33d73ba2fda6fd
MayorGee <adebayomayowa78@gmail.com>
2021-12-07 15:46:34 +0300

    updated error page

--- src/Pages/404/404.js
+++ src/Pages/404/404.js
@@ -1,39 +1,32 @@
 import { NavLink } from "react-router-dom";
-import Lottie from 'lottie-react-web'
-
-import NotFoundAnimation from '../../assets/lottie/404.json';
-
 import Header from "../../Components/Header/Header";
-import './404.css';
+import "./404.css";
+import React from "react";
+
+
 
 const NotFoundPage = () => {
-    return (
-        <>
-            {<Header/>}
-            <div className='wrapper-main-not-found'>
-                <div className='wrapper-lottie'>
-                    <Lottie
-                        options={{
-                            animationData: NotFoundAnimation
-                        }}
-                    />
-                </div>
 
-                <span className='title-not-found'>
-                    Что-то пошло не так
-                </span>
+    
+  return (
+    <>
+      {<Header />}
+      <div className="wrapper-main-not-found">
+        <div className="wrapper-lottie">
+          <img src="/images/image-404.gif" className="image-404" alt=""/>
+        </div>
 
-                <p className='summary-not-found'>
-                    Кажется, что страница которую вы ищите, была перемещена,
-                    переименована или даже удалена!
-                </p>
+        <span className="title-not-found">Page not found</span>
 
-                <NavLink to='/'>
-                    <button className="button-back"><span>Вернуться</span></button>
-                </NavLink>
-            </div>
-        </>
-    );
-}
+        <p className="summary-not-found">
+          Seems you have lost your way. Let's take you back to the
+          <NavLink to="/">
+            <a className="link-back"> main page</a>
+          </NavLink>
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default NotFoundPage;
