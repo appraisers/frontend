3e7b40c5269a27f67db81d959d4aec34de7f10b4
MayorGee <adebayomayowa78@gmail.com>
2021-12-15 14:29:16 +0300

    more chages

--- src/App.js
+++ src/App.js
@@ -3,29 +3,31 @@ import { Switch, Route } from "react-router-dom";
 
 import RegisterPage from "./Pages/LoginPages/SignUpPage/SignUpFirstPage";
 import SecondRegisterPage from "./Pages/LoginPages/SignUpPage/SignUpSecondPage";
-import LoginPage from "./Pages/LoginPages/SignInPage";
-import RestorePassword from './Pages/LoginPages/RestorePassword/RestorePasswordFirstPage';
-import SecondPasswordPage from "./Pages/LoginPages/RestorePassword/RestorePasswordSecondPage";
+import SignIn from "./Pages/LoginPages/SignInPage/SignInPage";
+import RestorePasswordFirstPage from "./Pages/LoginPages/RestorePassword/RestorePasswordFirstPage";
+import RestorePasswordSecondPage from "./Pages/LoginPages/RestorePassword/RestorePasswordSecondPage";
 
-import Main from './Pages/HomePage';
-import About from './Pages/AboutPage';
-import PageNotFound from "./Pages/404";
+import HomePage from "./Pages/HomePage/HomePage";
+import AboutPage from "./Pages/AboutPage/AboutPage";
+import NotFoundPage from "./Pages/404/404";
 
 const App = () => {
     return (
         <div className="App">
             <Switch>
-                <Route path='/' exact component={Main} />
-                <Route path="/login" component={LoginPage} />
+                <Route path='/' exact component={HomePage} />
+                <Route path="/login" component={SignIn} />
                 <Route path="/sign/up" exact component={RegisterPage} />
                 <Route path="/sign/up/2" component={SecondRegisterPage} />
-                <Route path="/forgot_password" component={RestorePassword} />
-                <Route path="/forgot_password_2" component={SecondPasswordPage} />
-                <Route path='/about' component={About} />
-                <Route component={PageNotFound} />
+                <Route path="/forgot_password" component={RestorePasswordFirstPage} />
+                <Route path="/forgot_password_2" component={RestorePasswordSecondPage} />
+                <Route path='/about' component={AboutPage} />
+                <Route component={NotFoundPage} />
             </Switch>
         </div>
     );
 };
 
+
 export default App;
+
