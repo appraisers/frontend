438f5a22ec5e986fd11ee284b542b9029261f64f
KirushaGIT <kirill.garnov@websailors.pro>
2021-12-18 19:47:00 +0300

    prepared code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import RegisterPage from "./Pages/LoginPages/SignUpPage/SignUpFirstPage";
-import SecondRegisterPage from "./Pages/LoginPages/SignUpPage/SignUpSecondPage";
-import LoginPage from "./Pages/LoginPages/SignInPage";
 import RestorePassword from './Pages/LoginPages/RestorePassword/RestorePasswordFirstPage';
 import SecondPasswordPage from "./Pages/LoginPages/RestorePassword/RestorePasswordSecondPage";
 
@@ -16,9 +13,6 @@ const App = () => {
         <div className="App">
             <Switch>
                 <Route path='/' exact component={Main} />
-                <Route path="/login" component={LoginPage} />
-                <Route path="/sign/up" exact component={RegisterPage} />
-                <Route path="/sign/up/2" component={SecondRegisterPage} />
                 <Route path="/forgot_password" component={RestorePassword} />
                 <Route path="/forgot_password_2" component={SecondPasswordPage} />
                 <Route path='/about' component={About} />
