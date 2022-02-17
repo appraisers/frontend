60d9d8819ffdb46f643d75fdac31c6971c5933bf
Kirill <57805489+KirushaGIT@users.noreply.github.com>
2022-02-17 15:52:58 +0300

    Update index.js

--- src/Pages/SurveyPage/index.js
+++ src/Pages/SurveyPage/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory, NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 import AlertHelper from '../../Components/Alert';
