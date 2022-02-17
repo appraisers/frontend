c4f6cd6bcdcfb42aa337d557c6b08b087bd2a915
Grigory <ggarnov@mail.ru>
2022-02-17 14:54:59 +0300

    added BE for add answer

--- src/Components/Rating/index.js
+++ src/Components/Rating/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import Rate from 'rc-rate';
 import 'rc-rate/assets/index.css';
 import './Rating.css';
- 
-const StarRating = () => {
-  return (
-    <Rate />
-  );
-}
-  
+
+const StarRating = ({ onChange }) => {
+  return <Rate onChange={onChange} />;
+};
+
 export default StarRating;
