4b3efb226b15543a6e34fa511e9f253a69315a7a
Grigory <ggarnov@mail.ru>
2022-02-26 18:16:34 +0300

    added header on main page

--- src/Pages/AccountPage/index.js
+++ src/Pages/AccountPage/index.js
@@ -1,11 +1,12 @@
-import MainHeader from "../../Components/MainHeader";
+import AuthorizedHeader from '../../Components/AuthorizedHeader';
 
 import "./AccountPage.scss";
 
 const AccountPage = () => {
   return (
-    <div>
-      <MainHeader />
+    <div className='account-page-main'>
+     <AuthorizedHeader className="account-page-header" title="Личный кабинет" />
+
       <div className="account-page-body">
         <h1>Личный кабинет</h1>
       </div>
