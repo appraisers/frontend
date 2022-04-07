c8d7b5a21b9161a01f35871c8bf4d43d9ae40cd4
KirushaGIT <kirill.garnov@websailors.pro>
2022-04-07 20:44:16 +0300

    added default title for user info

--- src/Components/SimpleModalUsers/index.js
+++ src/Components/SimpleModalUsers/index.js
@@ -27,59 +27,66 @@ const SimpleModalTableHelper = ({ selectedUser, onClose }) => {
           <h1 className="simple-modal-users-username">
             {selectedUser.fullname}
           </h1>
-          <h3 className='simple-modal-users-header'>Прохождение опросов</h3>
+          <h3 className="simple-modal-users-header">Прохождение опросов</h3>
 
-          <TableContainer
-            component={Paper}
-            className="simple-modal-table-container"
-          >
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell
-                    className="simple-modal-table-header"
-                    align="center"
-                  >
-                    Имя
-                  </TableCell>
-                  <TableCell
-                    className="simple-modal-table-header"
-                    align="center"
-                  >
-                    Дата
-                  </TableCell>
-                  <TableCell
-                    className="simple-modal-table-header"
-                    align="center"
-                  >
-                    Рейтинг
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {selectedUser.review.map((review) => (
-                  <StyledTableRow key={review.id}>
+          {selectedUser.review?.length > 0 ? (
+            <TableContainer
+              component={Paper}
+              className="simple-modal-table-container"
+            >
+              <Table>
+                <TableHead>
+                  <TableRow>
                     <TableCell
+                      className="simple-modal-table-header"
                       align="center"
-                      className="all-users-table-cell-username"
                     >
-                      {review.description}
+                      Имя
                     </TableCell>
-                    <TableCell align="center">
-                      {review.createdAt
-                        ? format(new Date(review.createdAt), 'dd-MM-yyyy HH:MM')
-                        : '-'}
+                    <TableCell
+                      className="simple-modal-table-header"
+                      align="center"
+                    >
+                      Дата
                     </TableCell>
-                    <TableCell align="center">
-                      {review.ratingSummary
-                        ? review.ratingSummary.toFixed(1)
-                        : '-'}
+                    <TableCell
+                      className="simple-modal-table-header"
+                      align="center"
+                    >
+                      Рейтинг
                     </TableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {selectedUser.review.map((review) => (
+                    <StyledTableRow key={review.id}>
+                      <TableCell
+                        align="center"
+                        className="all-users-table-cell-username"
+                      >
+                        {review.description}
+                      </TableCell>
+                      <TableCell align="center">
+                        {review.createdAt
+                          ? format(
+                              new Date(review.createdAt),
+                              'dd-MM-yyyy HH:MM'
+                            )
+                          : '-'}
+                      </TableCell>
+                      <TableCell align="center">
+                        {review.ratingSummary
+                          ? review.ratingSummary.toFixed(1)
+                          : '-'}
+                      </TableCell>
+                    </StyledTableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          ) : (
+            <h1>Нет отзывов</h1>
+          )}
         </div>
 
         <div className="simple-modal-user-pie-chart">
